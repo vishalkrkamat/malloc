7eae73c93cc58e9c567e8ba1b4526ce4f701dcad
vishalkrkamat <vishalkrkamat7@gmail.com>
2026-01-17 14:11:18 +0530

    boundary tag footer introduced and overall code changes

--- src/main.c
+++ src/main.c
@@ -7,21 +7,27 @@
 #define ALIGNMENT 16
 #define ALIGN(x) (((x) + (ALIGNMENT - 1)) & ~(ALIGNMENT - 1))
 #define HEADER_SIZE ALIGN(sizeof(block))
+#define FOOTER_SIZE ALIGN(sizeof(footer))
+#define BLOCK_OVERHEAD (HEADER_SIZE + FOOTER_SIZE)
+#define MIN_BLOCK_SIZE (BLOCK_OVERHEAD + MIN_PAYLOAD)
 
 typedef struct block {
-    size_t payload_size;
+    size_t size;
     int free;
     struct block *next;
 } block;
 
-static block *block_list = NULL;
+typedef block footer;
 
-void test_split(void);
+static block *free_list = NULL;
+
 void *myalloc(size_t size);
 block *find_free_block(size_t size);
 void release_block(void *ptr);
 void *current_memory_break();
 void split_block(block *ptr, size_t size);
+void remove_from_free_list(block *b);
+void insert_into_free_list(block *b);
 
 int main() {
 
@@ -35,49 +41,54 @@ int main() {
     printf("%ld\n", sizeof(*user_data));
 
     block *header = (block *)(((char *)user_data) - HEADER_SIZE);
-    printf("%ld\n", header->payload_size);
+    printf("%ld\n", header->size);
 
     printf("Current memory break: %p\n", current_memory_break());
     release_block(user_data);
-    test_split();
     return 0;
 }
 
 void *myalloc(size_t size) {
     size_t payload_size = ALIGN(size);
+    size_t total_size = payload_size + BLOCK_OVERHEAD;
 
-    block *free_block = find_free_block(payload_size);
+    block *free_block = find_free_block(total_size);
     if (free_block) {
-        if (free_block->payload_size >=
-            payload_size + HEADER_SIZE + MIN_PAYLOAD) {
+        remove_from_free_list(free_block);
+        if (free_block->size >= total_size + MIN_BLOCK_SIZE) {
             split_block(free_block, payload_size);
         }
         free_block->free = 0;
-        printf("found free momory\n");
+
+        footer *f =
+            (footer *)((char *)free_block + free_block->size - FOOTER_SIZE);
+        f->free = 0;
+
         return (void *)((char *)free_block + HEADER_SIZE);
     }
 
-    void *rawmem = sbrk(HEADER_SIZE + payload_size);
+    void *rawmem = sbrk(total_size);
     if (rawmem == (void *)-1) {
         errno = ENOMEM;
         return NULL;
     }
 
-    printf("Allocating new one\n");
     block *new_allocated_block = (block *)rawmem;
-    new_allocated_block->payload_size = payload_size;
+    new_allocated_block->size = total_size;
     new_allocated_block->free = 0;
-    new_allocated_block->next = block_list;
-    block_list = new_allocated_block;
 
+    footer *ftr =
+        (footer *)((char *)new_allocated_block + total_size - FOOTER_SIZE);
+    ftr->size = total_size;
+    ftr->free = 0;
     return (void *)((char *)new_allocated_block + HEADER_SIZE);
 }
 
 block *find_free_block(size_t size) {
 
-    block *curr = block_list;
+    block *curr = free_list;
     while (curr) {
-        if (curr->free && curr->payload_size >= size) {
+        if (curr->free && curr->size >= size) {
             return curr;
         }
         curr = curr->next;
@@ -85,19 +96,56 @@ block *find_free_block(size_t size) {
     return NULL;
 }
 
+void remove_from_free_list(block *b) {
+    if (!b || !free_list)
+        return;
+
+    if (free_list == b) {
+        free_list = b->next;
+        return;
+    }
+
+    block *curr = free_list;
+    while (curr->next && curr->next != b)
+        curr = curr->next;
+
+    if (curr->next == b)
+        curr->next = b->next;
+}
+
 void split_block(block *ptr, size_t requested_payload) {
 
     if (!ptr || !ptr->free)
         return;
 
-    block *new_block = (block *)((char *)ptr + HEADER_SIZE + requested_payload);
-    new_block->payload_size =
-        ptr->payload_size - HEADER_SIZE - requested_payload;
-    new_block->free = 1;
-    new_block->next = ptr->next;
+    size_t allocated_size = requested_payload + BLOCK_OVERHEAD;
+    size_t remaining_size = ptr->size - allocated_size;
 
-    ptr->payload_size = requested_payload;
-    ptr->next = new_block;
+    if (remaining_size < MIN_BLOCK_SIZE) {
+        return;
+    }
+
+    block *new_block = (block *)((char *)ptr + allocated_size);
+    new_block->size = remaining_size;
+    new_block->free = 1;
+
+    footer *new_block_footer =
+        (footer *)((char *)new_block + remaining_size - FOOTER_SIZE);
+    new_block_footer->size = remaining_size;
+    new_block_footer->free = 1;
+
+    ptr->size = allocated_size;
+    ptr->free = 0;
+
+    footer *ptr_footer = (footer *)((char *)ptr + allocated_size - FOOTER_SIZE);
+    ptr_footer->size = allocated_size;
+    ptr_footer->free = 0;
+    insert_into_free_list(new_block);
+}
+
+void insert_into_free_list(block *b) {
+    b->next = free_list;
+    free_list = b;
 }
 
 void release_block(void *ptr) {
@@ -111,6 +159,12 @@ void release_block(void *ptr) {
         abort();
 
     mem->free = 1;
+
+    footer *ftr = (footer *)((char *)ptr + mem->size - FOOTER_SIZE);
+
+    ftr->free = 1;
+
+    insert_into_free_list(mem);
 }
 
 void *current_memory_break() {
@@ -119,21 +173,4 @@ void *current_memory_break() {
         return NULL;
     }
     return p;
-}
-
-void test_split(void) {
-    void *p1 = myalloc(128);
-    release_block(p1);
-
-    void *brk_before = current_memory_break();
-    void *p2 = myalloc(32);
-    void *brk_after = current_memory_break();
-
-    block *b1 = (block *)((char *)p2 - HEADER_SIZE);
-    block *b2 = b1->next;
-
-    printf("=== SPLIT TEST ===\n");
-    printf("b1 payload: %zu\n", b1->payload_size);
-    printf("b2 payload: %zu\n", b2->payload_size);
-    printf("brk changed: %d\n", brk_before != brk_after);
 }
